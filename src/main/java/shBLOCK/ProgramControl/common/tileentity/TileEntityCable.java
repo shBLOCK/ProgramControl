6c7531147d189617690c0f9842274cd9e58b8cf9
shBLOCK <3332908658@qq.com>
2021-01-10 18:13:58 +0800

    test

--- src/main/java/shBLOCK/ProgramControl/common/tileentity/TileEntityCable.java
+++ src/main/java/shBLOCK/ProgramControl/common/tileentity/TileEntityCable.java
@@ -1,6 +1,11 @@
 package shBLOCK.ProgramControl.common.tileentity;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 
-public class TileEntityCable extends TileEntity {
+public class TileEntityCable extends TileEntity implements ITickable {
+    @Override
+    public void update() {
+
+    }
 }
